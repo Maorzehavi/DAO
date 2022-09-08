8f7404e1f665839ed9cf3a30d06d691d0a33920a
Maor Zehavi <103786336+Maorzehavi@users.noreply.github.com>
2022-09-08 22:00:25 +0300

    Update ObjectDAO.java

--- data/ObjectDAO.java
+++ data/ObjectDAO.java
@@ -1,29 +1,11 @@
 package data;
 
-import java.io.Serializable;
 import java.sql.*;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
 
-public abstract class ObjectDAO<T> implements DAO<T>, Serializable {
+public abstract class ObjectDAO<T> implements DAO<T> {
 
-
-    public static Map<String ,String> getDatabase(String databaseName) {
-        Map<String, String> map = new HashMap<>();
-        try (Connection connection = getConnection(databaseName);
-            PreparedStatement ps = connection.prepareStatement("show databases")) {
-            ResultSetMetaData rs = ps.executeQuery().getMetaData();
-            for (int i = 1; i <= rs.getColumnCount(); i++) {
-                map.put(rs.getColumnName(i), rs.getColumnTypeName(i));
-            }
-            return map;
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-      }
-        return null;
-    }
 
     @Override
     public boolean delete(String databaseName, String tableName, int id) {
@@ -42,6 +24,7 @@ public abstract class ObjectDAO<T> implements DAO<T>, Serializable {
 
     /**
      * this method is used to get the primary key name of a table in a database or null if the table does not exist
+     *
      * @param databaseName
      * @param tableName
      * @return primary key name of table
@@ -63,6 +46,7 @@ public abstract class ObjectDAO<T> implements DAO<T>, Serializable {
 
     /**
      * get a connection to the database
+     *
      * @param databaseName
      * @return a connection to the database or null if the connection fails
      */
@@ -71,17 +55,39 @@ public abstract class ObjectDAO<T> implements DAO<T>, Serializable {
         String user = "root";
         String password = "root";
         Connection con = null;
+
         try {
             con = DriverManager.getConnection(url, user, password);
+            con.setAutoCommit(false);
         } catch (SQLException e) {
             e.printStackTrace();
+            try {
+                assert con != null;
+                con.rollback();
+            } catch (SQLException ex) {
+                e.printStackTrace();
+            }
+        } finally {
+            try {
+                assert con != null;
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
+        try {
+            con.commit();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         return con;
     }
 
 
     /**
      * a method to print all the objects in the table
+     *
      * @param databaseName
      * @param tableName
      */
@@ -99,9 +105,10 @@ public abstract class ObjectDAO<T> implements DAO<T>, Serializable {
 
     /**
      * create a table in the database
+     *
      * @param databaseName
      * @param tableName
-     * @param "the sql statement to create the table"
+     * @param "the         sql statement to create the table"
      * @return true if the table was created, false otherwise
      */
     public static boolean createTable(String databaseName, String tableName, String... ts) {
